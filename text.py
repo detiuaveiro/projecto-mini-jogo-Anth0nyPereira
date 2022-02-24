51806b15a01b6ef673c9c00e50dd47e33efd7060
Anth0nyPereira <anthonypereira@ua.pt>
2022-02-24 22:38:12 +0000

    documented code from text classes

--- text.py
+++ text.py
@@ -5,9 +5,33 @@ from consts import Consts
 
 
 class Text(ABC):
+    """
+    An abstract class used to represent a kind of text that needs to be displayed on screen
+    """
 
     @abstractmethod
     def __init__(self, pos_x, pos_y, msg, font_style, size, color):
+
+        """
+        Parameters
+        ----------
+
+        pos_x: int
+               the first coordinate of the position of the text
+
+        pos_y: int
+                the second coordinate of the position of the text
+
+        msg: str
+                the message that is going to be displayed
+
+        font_style: str
+                the font that will be used
+                
+        color: str
+                the color that will be used
+        """
+
         # initializing attributes
         self.pos_x = pos_x
         self.pos_y = pos_y
@@ -25,6 +49,9 @@ class Text(ABC):
 
 
 class ScoreText(Text):
+    """
+    A concrete text class used to represent the actual score
+    """
 
     def __init__(self):
         self.score = 0
@@ -50,6 +77,10 @@ class ScoreText(Text):
 
 
 class GameOverText(Text):
+    """
+    A concrete text class used to represent the game over text
+    """
+
     def __init__(self):
         super().__init__(Consts.WIDTH // 2, 100, "GAME OVER", "michroma", 80, "red")
 
